e508aa1c40b5ac37d0fd01271ff26041db879ea2
toobi-jpeg <rex.98@live.se>
2025-10-02 15:31:51 +0200

    upload file modulerad. data från upload sätts nu i table state i app.jsx och skickas in till tabellen och export.

---
+++ src/components/UploadButton.jsx
@@ -0,0 +1,29 @@
+import UploadXLSX from '../utils/UploadXLSX';
+
+const UploadButton = ({ setTableData }) => {
+  const handleUpload = async (e) => {
+    let data = await UploadXLSX(e);
+
+    setTableData(data);
+  };
+
+  return (
+    <div>
+      <h3>Ladda upp en xlsx-fil (Excel/Google Sheet):</h3>
+      <label htmlFor="upload-file">
+        <input
+          style={{ display: 'none' }}
+          id="upload-file"
+          type="file"
+          accept=".xlsx,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+          onChange={handleUpload}
+        />
+        <Button variant="outlined" component="span">
+          Ladda upp fil
+        </Button>
+      </label>
+    </div>
+  );
+};
+
+export default UploadButton;
